0b9351c4d33edbdac88afed591eba51a8eac897a
Inbasekar <inbatech2024@gmail.com>
2025-02-26 13:02:33 +0530

    modifed root page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,5 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (router) {
-      router.replace("/Login"); // Use replace() to prevent history stacking
-    }
-  }, [router]);
-
-  return <p>Redirecting...</p>; // Display message while redirecting
+  redirect("/Login"); // Server-side redirect (better for SEO)
 }
