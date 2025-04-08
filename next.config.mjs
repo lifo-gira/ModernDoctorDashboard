1013f887951a96e1c395c82e048415365b3a42c4
Inbasekar <inbatech2024@gmail.com>
2025-04-09 00:08:12 +0530

    removed next export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // key line for static export in Next.js 13+
+    output: 'export',
   };
   
   export default nextConfig;
